bd709ae53151573ee89933aaca47c33059328fd7
ShivamPundir2859 <shivampundir2859@gmail.com>
2026-04-22 00:27:49 +0530

    added Pricing page

--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { appPlans } from '../assets/assets';
+
+interface Plan {
+  id: string;
+  name: string;
+  price: number;
+  credits: number;
+  description: string;
+  features: string[]; 
+}
 
 const Pricing = () => {
+  const [plans] = React.useState<Plan[]>(appPlans)
   return (
-    <div>Pricing</div>
+    <>
+      <div className='w-full max-w-5xl mx-auto z-20 max:md:px-4 min-h-[80vh]'>
+        <div className='text-center mt-16'>
+          <h1 className='text-gray-100 text-3xl font-medium'>Choose Your Plan</h1>
+          <p className='text-gray-400 text-sm max-w-md mx-auto mt-2'>Start for Free and Scale up
+            as you grow. Find the perfect plan for your content creation needs.
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
 
