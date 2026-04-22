1df486d7308d3b7bb269ac38aff35483b5f79a38
ShivamPundir2859 <shivampundir2859@gmail.com>
2026-04-23 01:23:22 +0530

    error solved

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,78 +1,109 @@
 import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams, Link } from 'react-router-dom'
 import type { Project } from '../types'
-import { Loader2Icon, MessageSquare, MessageSquareIcon, XIcon } from 'lucide-react'
+import { ArrowBigDownDashIcon, EyeIcon, EyeOff, EyeOffIcon, FullscreenIcon, Laptop, LaptopIcon, Loader2Icon, MessageSquare, MessageSquareIcon, SaveIcon, Smartphone, SmartphoneIcon, Tablet, TabletIcon, XIcon } from 'lucide-react'
 import { dummyConversations, dummyProjects } from '../assets/assets'
 
 const Projects = () => {
-  const {ProjectId} = useParams()
+  const { projectId } = useParams()
   const navigate = useNavigate()
 
   const [Project, setProject] = useState<Project | null>(null)
   const [loading, setLoading] = useState(true)
 
-  const[isGenrating, setIsGenrating] = useState(true)
-  const[device, setDevice] = useState<'phone'|'tablet'|'desktop'>("desktop")
+  const [isGenrating, setIsGenrating] = useState(true)
+  const [device, setDevice] = useState<'phone' | 'tablet' | 'desktop'>("desktop")
 
-  const[isMenuOpen, setIsMenuOpen] = useState(true)
-  const[isSaving, SetIsSaving] = useState(true)
+  const [isMenuOpen, setIsMenuOpen] = useState(true)
+  const [isSaving, SetIsSaving] = useState(true)
 
-  const fetchProject = async() =>{
-    const project = dummyProjects.find(project => projectid === projectId)
-    setTimeout(()=>{
-      if(project){
-        setProject({...project, conversation: dummyConversations});
+  const fetchProject = async () => {
+    const project = dummyProjects.find(project => project.id === projectId)
+    setTimeout(() => {
+      if (project) {
+        setProject({ ...project, conversation: dummyConversations });
         setLoading(false)
         setIsGenrating(project.current_code ? false : true)
+      } else {
+        setLoading(false)
       }
-    })
+    }, 2000)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchProject()
-  },[])
+  }, [])
 
-  if(loading){
+  if (loading) {
     return (
       <>
         <div className='flex items-center justify-center h-screen'>
-          <Loader2Icon className='size-7 animate-spin text-violet-200'/>
+          <Loader2Icon className='size-7 animate-spin text-violet-200' />
         </div>
       </>
     )
   }
 
-  return project ? (
+  return Project ? (
     <div className='flex flex-col h-screen w-full bg-gray-900 text-white'>
       {/* {Builder Navbar} */}
       <div className='flex max-sm:flex-col sm:items-center gap-4 px-4 py-2 no-scrollbar'>
         {/* <left */}
         <div className='flex items-center gap-2 sm:min-w-90 text-nowrap'>
-          <img src="/favicon.svg" alt="logo" className='h-6 cursor-pointer' onClick={()=> navigate('/')}/>
+          <img src="/favicon.svg" alt="logo" className='h-6 cursor-pointer' onClick={() => navigate('/')} />
           <div className='max-w-64 sm:max-w-xs'>
             <p className='text-sm text-medium capitalize truncate'>{Project?.name}</p>
             <p className='text-xs text-gray-400 -mt-0.5'>Previewing last saved version</p>
           </div>
           <div className='sm:hidden flex-1 flex justify-end'>
-            {isMenuOpen ? 
-            <MessageSquareIcon onClick={()=> setIsMenuOpen(false)} className='size-6 cursor-pointer'/>
-            : <XIcon onClick={()=> setIsMenuOpen(true)} className='size-6 cursor-pointer' />}
+            {isMenuOpen ?
+              <MessageSquareIcon onClick={() => setIsMenuOpen(false)} className='size-6 cursor-pointer' />
+              : <XIcon onClick={() => setIsMenuOpen(true)} className='size-6 cursor-pointer' />}
           </div>
         </div>
 
         {/* Middle */}
-        <div></div>
+        <div className='hidden sm:flex gap-2 bg-gray-950 p-1.5 rounded-md'>
+          <SmartphoneIcon onClick={() => setDevice('phone')} className={`size-6 p-1 rounded cursor-pointer 
+            ${device === 'phone' ? "bg-gray-700" : ""}`} />
+          <TabletIcon onClick={() => setDevice('tablet')} className={`size-6 p-1 rounded cursor-pointer 
+            ${device === 'tablet' ? "bg-gray-700" : ""}`} />
+          <LaptopIcon onClick={() => setDevice('desktop')} className={`size-6 p-1 rounded cursor-pointer 
+            ${device === 'desktop' ? "bg-gray-700" : ""}`} />
+        </div>
         {/* Right */}
-        <div></div>
+        <div className='flex items-center justify-end gap-3 flex-1 text-xs sm:text-sm'>
+            <button disabled={isSaving} className='max-sm:hidder bg-gray-800 hover:bg-gray-700 text-white px-3.5
+            py-1 flex items-center gap-2 rounded sm:rounded-sm transition-colors border border-gray-700'>
+              {isSaving ? <Loader2Icon className='animate-spin' size={16}/> :
+              <SaveIcon size={16}/>}
+                Save
+            </button>
+            <Link target='_blank' to={`/preview/${projectId}`} className='flex items-center gap-2 px-4
+              py-1 rounded sm:rounded-sm border border-gray-700 hover:border-gray-500 transition-colors'>
+              <FullscreenIcon size={16}/>Preview
+            </Link>
+            <button className='bg-linear-to-br from-blue-700 to-blue-600 hover:from-blue-600
+             hover:to-blue-500 text-white px-3.5 py-1 flex items-center gap-2 rounded
+             sm:rounded-sm transition-colors'>
+              <ArrowBigDownDashIcon size={16}/>Download
+            </button>
+            <button>
+              {Project.isPublished ?
+              <EyeOffIcon size={16}/> : <EyeIcon size={16}/>
+              }
+              {Project.isPublished ? "Unpublish" : "Publish"}
+            </button>
+        </div>
       </div>
-      
+
     </div>
-  ):
-  (
-    <div className='flex items-center justify-center h-screen'>
-    <p className='text-2xl font-medium text-gray-200'>Unable to load project!</p>
-    </div>
-  )
+  ) :
+    (
+      <div className='flex items-center justify-center h-screen'>
+        <p className='text-2xl font-medium text-gray-200'>Unable to load project!</p>
+      </div>
+    )
 }
 
 export default Projects
