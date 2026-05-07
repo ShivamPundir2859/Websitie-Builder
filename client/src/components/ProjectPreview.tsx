164a42983bbb107e11b86c46a377477aad842b98
ShivamPundir2859 <shivampundir2859@gmail.com>
2026-05-07 15:01:30 +0530

    todays changes

--- client/src/components/ProjectPreview.tsx
+++ client/src/components/ProjectPreview.tsx
@@ -1,5 +1,6 @@
 import React, { forwardRef } from 'react'
 import type { Project } from '../types';
+import { iframeScript } from '../assets/assets';
 
 interface ProjectPreviewProps{
    project: Project;
@@ -14,11 +15,28 @@ export interface ProjectPreviewRef{
 
 const ProjectPreview = forwardRef<ProjectPreviewRef, ProjectPreviewProps>(
    ({project, isGenrating, device = 'desktop', showEditorPanel = true}, ref) => {
+
+      const iframeRef = useRef<HTMLFrameElement>(null)
+      const injectPreview = (html: string) =>{
+         if(!html)return '';
+         if(!showEditorPanel) return html
+         if(html.includes('</body>')){
+            return html.replace('</body>', iframeScript + '</body>')
+         }else{
+            return html + iframeScript
+         }
+      }
+
   return (
     <div className='relative h-full bg-gray-900 flex-1 rounded-xl overflow-hidden max-sm:ml-2'>
       {project.current_code? (
          <>
+         <iframe 
+         ref={iframeRef}
+         srcDoc={injectPreview(project.current_code)}/>
          </>
+      ): isGenrating&& (
+         <div>loading</div>
       )}
     </div>
   )
