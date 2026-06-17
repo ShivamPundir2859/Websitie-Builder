858433079c256875ac6c1f75924f909008ef8276
ShivamPundir2859 <shivampundir2859@gmail.com>
2026-06-17 09:44:05 +0530

    added the back-end serever

---
+++ server/server.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from "cors";
+
+const app = express();
+
+
+const CorsOptions = {
+    origin: process.env.TRUSTED_ORIGINS?.split(',') || [],
+    Credential: true,
+}
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
